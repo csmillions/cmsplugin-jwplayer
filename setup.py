899c91f44b077772c062d087c40b78f4f2729281
Chris Modjeska <kurisui@gmail.com>
2013-12-13 10:25:40 -0500

    Added dependencies

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from setuptools import setup, find_packages
-import cmsplugin_javascript
+import cmsplugin_jwplayer
 
 setup(
-	name = "cmsplugin-javascript",
-	version = cmsplugin_javascript.__version__,
+	name = "cmsplugin-jwplayer",
+	version = cmsplugin_jwplayer.__version__,
 	packages = find_packages(),
 
 	author = "Chris Modjeska",
 	author_email = "kin@remuria.net",
 	description = "",
 	keywords = "django django-cms",
-	url = "https://github.com/csmillions/cmsplugin-javascript/",
+	url = "https://github.com/csmillions/cmsplugin-jwplayer/",
 	include_package_data=True,
 	long_description= """
 	""",
 	install_requires=[
-        "django-ordered-model",
-    ],
+		"django-ordered-model",
+		"django",
+		"django-cms"
+	],
 )
